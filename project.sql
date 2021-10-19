4d60b244336f61ad68c8d4dba8ca24402962f58d
Krithika <goyalkrithika7@gmail.com>
2021-10-19 23:15:41 +0530

    some grant commands added

--- project.sql
+++ project.sql
@@ -1,4 +1,9 @@
-CREATE DATABASE AIMS;CREATE TABLE course_catalog (
+CREATE USER dean_acads WITH PASSWORD "iitropar";
+CREATE USER acads_office WITH PASSWORD "iitropar";
+
+CREATE DATABASE AIMS;
+
+CREATE TABLE course_catalog (
     course_id INT UNIQUE PRIMARY KEY,
     LTPSC INT [] NOT NULL,
     course_name VARCHAR(255) NOT NULL
@@ -25,15 +30,31 @@ CREATE TABLE student_past_semester_credits (
     second_last_semester INT
 );
 
-CREATE OR REPLACE FUNCTION student_registration(
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_catalog TO dean_acads;
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_catalog TO acads_office;
+
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_prereq TO dean_acads;
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_prereq TO acads_office;
+
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_offering TO dean_acads;
+GRANT SELECT, UPDATE, INSERT, DELETE ON course_offering TO acads_office;
+
+GRANT SELECT, UPDATE, INSERT, DELETE ON student_past_semester_credits TO dean_acads;
+GRANT SELECT, UPDATE, INSERT, DELETE ON student_past_semester_credits TO acads_office;
+
+CREATE OR REPLACE FUNCTION student_registration (
     IN entry_number VARCHAR(15)
 ) RETURNS VOID AS $$
 BEGIN
+
+    -- make a new user with student entry number
+    EXECUTE format ('CREATE USER %I WITH PASSWORD "iitropar";', entry_number);
+
     -- add this in past semester credits table
     INSERT INTO student_past_semester_credits VALUES (entry_number, NULL, NULL);
 
     -- make a table for past cources of this student
-    EXECUTE format(
+    EXECUTE format (
         'CREATE TABLE %I (
             course_id INT NOT NULL,
             semester INT NOT NULL,
@@ -43,28 +64,63 @@ BEGIN
     );
 
     -- make a table for current courses of this student
-    EXECUTE format(
+    EXECUTE format (
         'CREATE TABLE %I (
             course_id INT NOT NULL,
             semester INT NOT NULL,
             year INT NOT NULL
         );', 'student_current_courses_' || entry_number
     );
+    
+    -- make a table for ticket for this student
+    EXECUTE format (
+        'CREATE TABLE %I (
+            ticket_id VARCHAR(255) NOT NULL,
+            course_id VARCHAR(255) NOT NULL,
+            semester INT NOT NULL,
+            year INT NOT NULL,
+            status VARCHAR(255) NOT NULL
+        );', 'student_ticket_table_' || entry_number
+    );
+
+    EXECUTE format (
+        'GRANT SELECT ON %I TO %I', 
+        'student_past_courses_' || entry_number, entry_number
+    );
+
+    EXECUTE format (
+        'GRANT SELECT, INSERT, UPDATE, DELETE ON %I TO %I', 
+        'student_current_courses_' || entry_number, entry_number
+    );
+
+    EXECUTE format (
+        'GRANT SELECT, INSERT ON %I TO %I', 
+        'student_ticket_table_' || entry_number, entry_number
+    );
+
 END
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION faculty_registration(
+CREATE OR REPLACE FUNCTION faculty_registration (
     IN faculty_id INT
 ) RETURNS VOID AS $$
 BEGIN
 
+    -- make a new user with faculty id
+    EXECUTE format ('CREATE USER %I WITH PASSWORD "iitropar";', faculty_id);
+
     -- make a table for course offering of this faculty
-    EXECUTE format(
+    EXECUTE format (
         'CREATE TABLE %I (
             course_id VARCHAR(15) NOT NULL,
             semester INT NOT NULL,
             year INT NOT NULL
         );', 'course_offering_' || faculty_id
+    );
+
+    EXECUTE format (
+        'GRANT SELECT, UPDATE, INSERT, DELETE ON %I TO %I', 
+        'course_offering_' || faculty_id, faculty_id
     );
 
 END
@@ -81,7 +137,7 @@ BEGIN
     INSERT INTO course_offering VALUES (faculty_id, course_id, semester, year);
 
     -- add into the course offering table of the faculty
-    EXECUTE format(
+    EXECUTE format (
         'INSERT INTO %I VALUES (course_id, semester, year);',
         'course_offering' || '_' || faculty_id
     );
@@ -93,8 +149,14 @@ BEGIN
             branch VARCHAR(255) NOT NULL,
             year INT NOT NULL,
             cg INT
-    );', faculty_id || '_' || course_id || '_' || semester || '_' || year
+        );', faculty_id || '_' || course_id || '_' || semester || '_' || year
     );
+
+    EXECUTE format (
+        'GRANT SELECT, UPDATE, INSERT, DELETE ON %I TO %I', 
+        faculty_id || '_' || course_id || '_' || semester || '_' || year, faculty_id
+    );
+
 END
 $$ LANGUAGE plpgsql;
 
